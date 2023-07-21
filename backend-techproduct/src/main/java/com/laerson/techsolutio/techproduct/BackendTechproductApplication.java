eab4407c11442a198bca142b7fd7a3f5e3599851
laersonjr <laersonjr@gmail.com>
2023-07-21 19:46:10 -0300

    :tada: Projeto iniciado

---
+++ backend-techproduct/src/main/java/com/laerson/techsolutio/techproduct/BackendTechproductApplication.java
@@ -0,0 +1,13 @@
+package com.laerson.techsolutio.techproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTechproductApplication.class, args);
+	}
+
+}
---
+++ backend-techproduct/src/test/java/com/laerson/techsolutio/techproduct/BackendTechproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerson.techsolutio.techproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTechproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
