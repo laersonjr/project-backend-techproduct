9cdb2f66f130f4d0881f2c3f3832a6ae8dda82eb
laersonjr <laersonjr@gmail.com>
2023-07-21 19:49:38 -0300

    feat: (Entity) Criado entity User

---
+++ backend-techproduct/src/main/java/com/laerson/techsolutio/techproduct/domain/entity/User.java
@@ -0,0 +1,26 @@
+package com.laerson.techsolutio.techproduct.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "tb_user")
+public class User {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String password;
+
+}
